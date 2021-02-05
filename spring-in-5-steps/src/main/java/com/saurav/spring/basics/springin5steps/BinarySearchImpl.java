7bd985ae95eaf7843696f6c23410cfbc06a2bfdd
Saurav Ranjan <s.k.ranjan1997@gmail.com>
2021-02-05 18:40:10 +0530

    A dummy  binary search to understand tight coupling

---
+++ spring-in-5-steps/src/main/java/com/saurav/spring/basics/springin5steps/BinarySearchImpl.java
@@ -0,0 +1,15 @@
+package com.saurav.spring.basics.springin5steps;
+
+public class BinarySearchImpl {
+	
+	public int binarySearch(int[] num, int numToSearchFor) {
+		
+		//Implementing search logic
+		
+		
+		//search the array
+		
+		return 3;
+	}
+
+}
--- spring-in-5-steps/src/main/java/com/saurav/spring/basics/springin5steps/SpringIn5StepsApplication.java
+++ spring-in-5-steps/src/main/java/com/saurav/spring/basics/springin5steps/SpringIn5StepsApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringIn5StepsApplication {
 
 	public static void main(String[] args) {
+		
+		BinarySearchImpl binSearch  = new BinarySearchImpl();
+		
+		System.out.println(binSearch.binarySearch(new int[] {1,3,4,5,6,7,9}, 3));
 		SpringApplication.run(SpringIn5StepsApplication.class, args);
 	}
 
